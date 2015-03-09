e5ef68dd29b43c42dfb0c1046492548dca59ecfa
Jerad Shepherd <jshepherd@brickyardmarketing.com>
2015-03-09 17:00:44 -0400

    Remove placeholder <li>

--- js/listly.js
+++ js/listly.js
@@ -1,11 +1,24 @@
-function Listly() {
+var Listly = function() {
+
   function Listly() {
     this.tasks = [];
-
     var self = this;
+
+    $('form#new_task').submit(function(ev) {
+      ev.preventDefault();
+      var task_name = this.task_name.value;
+
+      // Add a list item to #tasks
+      var result = $('#tasks').append('<li class="list-group-item">' + task_name + '</li>');
+      this.task_name.value = '';
+      $(this.task_name).focus();
+
+      // Add the task name to tasks array
+      
+    });
   }
-  
-  return Listly();
-}
+
+  return Listly;
+}();
 
 var listly = new Listly();
