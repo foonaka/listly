dc2e1b3f79aa9eeda6149486f80f1b5147a729d7
Jerad Shepherd <jshepherd@brickyardmarketing.com>
2015-03-10 17:29:07 -0400

    Delete tasks from local storage consistently.

--- js/listly.js
+++ js/listly.js
@@ -5,9 +5,10 @@ var Listly = function() {
     self.tasks = [];
 
     function addTask(task_name) {
-      self.tasks.push(task_name);
+      var task = new Task({ name: task_name });
+      self.tasks.push(task);
       if (save()) {
-        appendToList(task_name);
+        appendToList(task);
         return true;
       }
       else {
@@ -15,13 +16,13 @@ var Listly = function() {
       }
     }
 
-    function appendToList(task_name) {
+    function appendToList(task) {
       // Grab a copy of the list item template
       var li = $('#list_item_template').clone();
       li.removeAttr('id');
 
       // Add task name to the <li> template
-      li.find('label').text(task_name);
+      li.find('label').text(task.name);
 
       // Unhide
       li.removeClass('hidden');
@@ -29,15 +30,7 @@ var Listly = function() {
       // Activate delete button
       li.find('.btn-danger').click(function() {
         //Remove from array
-        self.tasks.splice(self.tasks.indexOf(task_name), 1);
-
-        // $.each(self.tasks, function(index, current_task) {
-        //   if (current_task === task_name) {
-        //     self.tasks.splice(index, 1);
-        //     // Stop looking once we find a match
-        //     return false;
-        //   }
-        // });
+        self.tasks.splice(self.tasks.indexOf(task), 1);
 
         //Save the array to local storage
         save();
@@ -67,8 +60,8 @@ var Listly = function() {
     function load() {
       if (supportsLocalStorage() && localStorage.tasks) {
         self.tasks = JSON.parse(localStorage.tasks);
-        $.each(self.tasks, function(index, task_name) {
-          appendToList(task_name);
+        $.each(self.tasks, function(index, task) {
+          appendToList(task);
         });
       }
     }
